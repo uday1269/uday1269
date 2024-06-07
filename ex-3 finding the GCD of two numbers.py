73748c012c37254be748350c3296c5a613478440
uday1269 <udaykiranbs9010@gmail.com>
2024-06-07 20:45:32 +0530

    Delete ex-3 finding the GCD of two numbers.py

--- ex-3 finding the GCD of two numbers.py
+++
@@ -1,8 +0,0 @@
-def gcd_number(a,b):
-    if b==0:
-        return a
-    else :
-        return gcd_number(b , a % b )
-num1=int(input("Enter the first number :"))
-num2=int(input("Enter the second number :"))
-print(f'GCD = {gcd_number(num1,num2)}')
